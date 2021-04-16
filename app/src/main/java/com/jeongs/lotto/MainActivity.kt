8d101673ef37e67ed64ae5cd91401114c1831103
Jeong <qsz78547@naver.com>
2021-04-16 20:30:19 +0900

    코틀린 책 연습용 app

---
+++ app/src/main/java/com/jeongs/lotto/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.jeongs.lotto
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+
+}
