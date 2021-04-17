bf6d33c597d17689be6fee855dec84665277ab79
Jeong <qsz78547@naver.com>
2021-04-17 22:32:02 +0900

    출력page생성

--- app/src/main/java/com/jeongs/lotto/MainActivity.kt
+++ app/src/main/java/com/jeongs/lotto/MainActivity.kt
@@ -1,16 +1,32 @@
 package com.jeongs.lotto
 
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.AttributeSet
 import android.view.View
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val random_select:CardView =findViewById(R.id.random_select)
+        random_select.setOnClickListener {
+            //랜덤 찾기
+            val intent =Intent(this,ResultActivity::class.java)
+            startActivity(intent)
+        }
+        val constellation_select:CardView= findViewById(R.id.constellation_select)
+        constellation_select.setOnClickListener {
+            //별자리로 찾기
+        }
+        val user_select:CardView= findViewById(R.id.user_select)
+        user_select.setOnClickListener {
+            //이름으로 찾기
+        }
 
     }
 
