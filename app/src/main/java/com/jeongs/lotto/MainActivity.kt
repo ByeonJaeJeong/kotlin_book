f28771160207036aaa89a0f1123b502254038714
Jeong <qsz78547@naver.com>
2021-04-21 18:51:47 +0900

    버튼 동작실행 itent 오류

--- app/src/main/java/com/jeongs/lotto/MainActivity.kt
+++ app/src/main/java/com/jeongs/lotto/MainActivity.kt
@@ -1,3 +1,4 @@
+
 package com.jeongs.lotto
 
 import android.content.Context
@@ -6,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.AttributeSet
 import android.view.View
+import android.widget.Toast
 import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
@@ -16,18 +18,21 @@ class MainActivity : AppCompatActivity() {
         val random_select:CardView =findViewById(R.id.random_select)
         random_select.setOnClickListener {
             //랜덤 찾기
+            Toast.makeText(this,"랜덤찾기 실행",Toast.LENGTH_SHORT).show()
             val intent =Intent(this,ResultActivity::class.java)
             startActivity(intent)
         }
         val constellation_select:CardView= findViewById(R.id.constellation_select)
         constellation_select.setOnClickListener {
             //별자리로 찾기
+            Toast.makeText(this,"별자리로 찾기 실행",Toast.LENGTH_SHORT).show()
             val intent =Intent(this,ResultActivity::class.java)
             startActivity(intent)
         }
         val user_select:CardView= findViewById(R.id.user_select)
         user_select.setOnClickListener {
             //이름으로 찾기
+            Toast.makeText(this,"이름으로 찾기 실행",Toast.LENGTH_SHORT).show()
             val intent =Intent(this,ResultActivity::class.java)
             startActivity(intent)
         }
