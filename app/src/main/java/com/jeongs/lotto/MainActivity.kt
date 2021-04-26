171a54560ffe0a00234bf2495b0ed792c62c8aab
Jeong <qsz78547@naver.com>
2021-04-26 17:28:33 +0900

    2021-04-26 update

--- app/src/main/java/com/jeongs/lotto/MainActivity.kt
+++ app/src/main/java/com/jeongs/lotto/MainActivity.kt
@@ -9,6 +9,7 @@ import android.util.AttributeSet
 import android.view.View
 import android.widget.Toast
 import androidx.cardview.widget.CardView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,25 +20,60 @@ class MainActivity : AppCompatActivity() {
         random_select.setOnClickListener {
             //랜덤 찾기
             Toast.makeText(this,"랜덤찾기 실행",Toast.LENGTH_SHORT).show()
+
             val nextIntent : Intent =Intent(this,ResultActivity::class.java)
+
+            nextIntent.putIntegerArrayListExtra("result",ArrayList(getRandomLottoNumbers()))
             startActivity(nextIntent)
         }
         val constellation_select:CardView= findViewById(R.id.constellation_select)
         constellation_select.setOnClickListener {
             //별자리로 찾기
             Toast.makeText(this,"별자리로 찾기 실행",Toast.LENGTH_SHORT).show()
-            val intent =Intent(this,ResultActivity::class.java)
-            startActivity(intent)
+            val nextIntent =Intent(this,ConstellationActivity::class.java)
+            startActivity(nextIntent)
         }
         val user_select:CardView= findViewById(R.id.user_select)
         user_select.setOnClickListener {
             //이름으로 찾기
             Toast.makeText(this,"이름으로 찾기 실행",Toast.LENGTH_SHORT).show()
-            val intent =Intent(this,ResultActivity::class.java)
-            startActivity(intent)
+            val nextIntent =Intent(this,NameActivity::class.java)
+            startActivity(nextIntent)
         }
 
     }
 
 
+}
+fun getRandomLottoNumber() : Int {
+
+    return Random.nextInt(45)+1
+}
+
+fun getRandomLottoNumbers(): MutableList<Int>{
+    val lottoNumbers = mutableListOf<Int>()
+
+    for (i in 1..6){
+        var number= 0
+        //중복제거
+        do {
+            number= getRandomLottoNumber()
+            //랜덤번호 추출
+        }while (lottoNumbers.contains(number))
+        //추출한 번호와 numbers 값이 없을때 까지 반복
+        lottoNumbers.add(number)
+    }
+
+    return lottoNumbers
+}
+fun getShuffleLottoNumbers() :MutableList<Int>{
+    val list= mutableListOf<Int>()
+
+    for(number in 1..45){
+        list.add(number)
+    }
+
+    list.shuffle()
+
+    return list.subList(0,6)
 }
--- app/src/main/java/com/jeongs/lotto/ResultActivity.kt
+++ app/src/main/java/com/jeongs/lotto/ResultActivity.kt
@@ -2,10 +2,39 @@ package com.jeongs.lotto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import kotlin.random.Random
 
 class ResultActivity : AppCompatActivity() {
+    val lottoImageStartId= R.drawable.ball_01
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        val resultLabel = findViewById<TextView>(R.id.resultLabel)
+        //전달받은 결과 배열을 가져옴
+        val result = intent.getIntegerArrayListExtra("result")
+        //전달받은 결과가 있을경우 가져옴
+        result?.let {
+            updateLottoBallImage(result.sortedBy { it })
+        }
+    }
+    fun updateLottoBallImage(result:List<Int>){
+        //결과 사이즈가 6개 미만인 경우 에러가 발생할수 있음
+        if(result.size < 6) return
+        //6개 미만일경우 리턴
+        val imageView01 = findViewById<ImageView>(R.id.imageView01)
+        val imageView02 = findViewById<ImageView>(R.id.imageView02)
+        val imageView03 = findViewById<ImageView>(R.id.imageView03)
+        val imageView04 = findViewById<ImageView>(R.id.imageView04)
+        val imageView05 = findViewById<ImageView>(R.id.imageView05)
+        val imageView06 = findViewById<ImageView>(R.id.imageView06)
+        imageView01.setImageResource(lottoImageStartId+result[0]-1)
+        imageView02.setImageResource(lottoImageStartId+result[1]-1)
+        imageView03.setImageResource(lottoImageStartId+result[2]-1)
+        imageView04.setImageResource(lottoImageStartId+result[3]-1)
+        imageView05.setImageResource(lottoImageStartId+result[4]-1)
+        imageView06.setImageResource(lottoImageStartId+result[5]-1)
     }
 }
