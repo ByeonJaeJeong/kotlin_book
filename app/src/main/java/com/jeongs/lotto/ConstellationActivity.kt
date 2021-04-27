b7965c5205edfda0143f582641fcbe95dac49208
Jeong <qsz78547@naver.com>
2021-04-27 21:10:31 +0900

    update

--- app/src/main/java/com/jeongs/lotto/ConstellationActivity.kt
+++ app/src/main/java/com/jeongs/lotto/ConstellationActivity.kt
@@ -1,6 +1,8 @@
 package com.jeongs.lotto
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class ConstellationActivity : AppCompatActivity() {
@@ -8,8 +10,23 @@ class ConstellationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_constellation)
+        val nextButton : Button = findViewById(R.id.goResultBtn)
+        val nextIntent : Intent = Intent(this,ResultActivity::class.java)
 
+        nextButton.setOnClickListener {
+            nextIntent.putIntegerArrayListExtra("result", ArrayList(getShuffleLottoNumbers()))
+            startActivity(nextIntent)
+        }
+    }
+    fun getShuffleLottoNumbers() :MutableList<Int>{
+        val list= mutableListOf<Int>()
 
+        for(number in 1..45){
+            list.add(number)
+        }
 
+        list.shuffle()
+
+        return list.subList(0,6)
     }
 }
--- app/src/main/java/com/jeongs/lotto/NameActivity.kt
+++ app/src/main/java/com/jeongs/lotto/NameActivity.kt
@@ -1,6 +1,10 @@
 package com.jeongs.lotto
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 import androidx.appcompat.app.AppCompatActivity
 
@@ -11,5 +15,26 @@ class NameActivity : AppCompatActivity() {
         setContentView(R.layout.activity_name)
 
 
+        val nextButton:Button = findViewById(R.id.goButton)
+        val nameText: EditText= findViewById(R.id.editText)
+        nextButton.setOnClickListener{
+            val nextIntent : Intent = Intent(this,ResultActivity::class.java)
+            nextIntent.putExtra("name",nameText.toString())
+            nextIntent.putIntegerArrayListExtra("result",ArrayList(getShuffleLottoNumbers()))
+            startActivity(nextIntent)
+        }
+    }
+
+    fun getShuffleLottoNumbers() :MutableList<Int>{
+        val list= mutableListOf<Int>()
+
+        for(number in 1..45){
+            list.add(number)
+        }
+
+        list.shuffle()
+
+        return list.subList(0,6)
     }
 }
+
